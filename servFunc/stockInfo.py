d26022f8d431a9b842fa0b5603804e678a4f5180
kyeong-bin <kbpak98@naver.com>
2022-08-09 16:35:50 +0900

    ver 1.0.15

--- servFunc/stockInfo.py
+++ servFunc/stockInfo.py
@@ -20,6 +20,6 @@ def getStock():
     order_possible_cash = domestic_trade.get_balance()
     benefit_percent=evlu[0]['asst_icdc_erng_rt']
    # createModel.insertTradeinfo(account=account, pr_account=pr_account, order_possible_cash=order_possible_cash, benefit_percent=benefit_percent)
-    #total_asset=총 자산, asst_icdc = 총 수익률, evlu_amt = 평가금액 총합
+    #current_cash = 잔금, total_asset=총 자산, asst_icdc = 총 수익률, evlu_amt = 평가금액 총합
     res = dict({'current_cash':order_possible_cash,'total_asset':evlu[0]['tot_evlu_amt'],'asst_icdc':evlu[0]['asst_icdc_erng_rt'], 'evlu_amt':evlu[0]['evlu_amt_smtl_amt']})
     return jsonify({'data':res}) #(node.js 서버와 연결했을경우 적용)
