f943dd5a372241e87f0ec2e48a773448beb3b20b
kyeong-bin <kbpak98@naver.com>
2022-08-09 15:23:24 +0900

    ver 1.0.15

--- servFunc/stockInfo.py
+++ servFunc/stockInfo.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, make_response, request, Response
+from flask import Blueprint, make_response, request, Response, jsonify
 from korStock import stock
 import korStock as kor
 from domestic_trade_v_alpha import domestic_trade
 import time
-import json
 #import createModel
 
 blue_get = Blueprint('getInfo', __name__, url_prefix='/info')
@@ -29,4 +28,4 @@ def getStock():
    # createModel.insertTradeinfo(account=account, pr_account=pr_account, order_possible_cash=order_possible_cash, benefit_percent=benefit_percent)
     #total_asset=총 자산, asst_icdc = 총 수익률, evlu_amt = 평가금액 총합
     res = dict({'total_asset':evlu[0]['tot_evlu_amt'],'asst_icdc':evlu[0]['asst_icdc_erng_rt'], 'evlu_amt':evlu[0]['evlu_amt_smtl_amt']})
-    return json.dumps(res) #(node.js 서버와 연결했을경우 적용)
+    return jsonify({'data':res}) #(node.js 서버와 연결했을경우 적용)
