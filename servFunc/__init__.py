6b73399e28f6ccba1ce538419623c5b7f71630ab
kyeong-bin <kbpak98@naver.com>
2022-08-10 11:51:21 +0900

    ver 1.1

--- servFunc/__init__.py
+++ servFunc/__init__.py
@@ -3,6 +3,8 @@ from servFunc import stockInfo
 from servFunc import strat
 from servFunc import chartdata
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
 app.register_blueprint(stockInfo.blue_get)
 app.register_blueprint(strat.blue_switch)
 app.register_blueprint(chartdata.blue_chart)
