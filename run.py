a97feac835e800af3dfb57d0c9de942218fbc3dc
kyeong-bin <kbpak98@naver.com>
2022-08-16 15:34:09 +0900

    ver 1.2.2

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from flask_cors import CORS
 
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
